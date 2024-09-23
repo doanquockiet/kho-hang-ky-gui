e6fddd8bdddefe2cb3eaea863b6b65b5e67f5ffd
Hong Bao Bao <baohbce170912@fpt.edu.vn>
2024-09-23 23:11:50 +0700

    update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,79 +1,55 @@
 
 import './styleFooter.css' // Assuming you have external CSS for styles
-
+import img from '../../assets/logo.jpg'
 const Footer = () => {
   return (
     <footer>
       <div className="aw__d16wu053">
         <div className="aw__catkylo">
           <section className="aw__c9g90xh">
-            {/* Download App Section */}
-            <div className="aw__i1ce1r9u">
-              <p className="aw__h1klxoqy">TẢI ỨNG DỤNG CHỢ TỐT</p>
-              <div className="aw__i8yrc5u">
-                <div className="aw__i55qi11">
-                  <picture>
-                    <source type="image/webp" srcSet="https://static.chotot.com/storage/default/group-qr.webp" />
-                    <img alt="Chợ Tốt" className="aw__i1nbm5k4" src="https://static.chotot.com/storage/default/group-qr.jpeg" width="87" height="87" />
-                  </picture>
-                </div>
-                <div className="aw__i55qi11">
-                  <ul className="aw__d2kbcgu">
-                    <li className="aw__d1fbfq7g">
-                      <a className="aw__d3ltxdb" href="https://itunes.apple.com/us/app/chotot.vn/id790034666" target="_blank" rel="noopener noreferrer nofollow">
-                        <img alt="App Store" src="https://static.chotot.com/storage/default/ios.svg" className="aw__d1kilyzx" />
-                      </a>
-                    </li>
-                    <li className="aw__d1fbfq7g">
-                      <a className="aw__d3ltxdb" href="https://play.google.com/store/apps/details?id=com.chotot.vn" target="_blank" rel="noopener noreferrer nofollow">
-                        <img alt="Google Play" src="https://static.chotot.com/storage/default/android.svg" className="aw__d1kilyzx" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+           
+           
 
             {/* Support Section */}
             <div className="aw__i1ce1r9u">
               <p className="aw__h1klxoqy">Hỗ trợ khách hàng</p>
               <ul className="aw__d2kbcgu">
                 <li className="aw__d1fbfq7g">
-                  <a className="aw__d3ltxdb" href="https://trogiup.chotot.com" target="_blank" rel="noopener noreferrer nofollow">Trung tâm trợ giúp</a>
+                  <a className="aw__d3ltxdb" href="#" target="_blank" rel="noopener noreferrer nofollow">Trung tâm trợ giúp</a>
                 </li>
                 <li className="aw__d1fbfq7g">
-                  <a className="aw__d3ltxdb" href="https://trogiup.chotot.com/nguoi-mua/cho-tot-bao-ve-nguoi-dung-nhu-the-nao/" target="_blank" rel="noopener noreferrer nofollow">An toàn mua bán</a>
+                  <a className="aw__d3ltxdb" href="#" target="_blank" rel="noopener noreferrer nofollow">An toàn mua bán</a>
                 </li>
                 <li className="aw__d1fbfq7g">
-                  <a className="aw__d3ltxdb" href="https://trogiup.chotot.com/lien-he" target="_blank" rel="noopener noreferrer nofollow">Liên hệ hỗ trợ</a>
+                  <a className="aw__d3ltxdb" href="#" target="_blank" rel="noopener noreferrer nofollow">Liên hệ hỗ trợ</a>
                 </li>
               </ul>
             </div>
 
             {/* About Section */}
             <div className="aw__i1ce1r9u">
-              <p className="aw__h1klxoqy">VỀ CHỢ TỐT</p>
+              <p className="aw__h1klxoqy">VỀ KHO HÀNG KÝ GỬI</p>
               <ul className="aw__d2kbcgu">
                 <li className="aw__d1fbfq7g">
-                  <a className="aw__d3ltxdb" href="https://trogiup.chotot.com/quy-dinh/gioi-thieu-chotot-com/" target="_blank" rel="noopener noreferrer nofollow">Giới thiệu</a>
+                  <a className="aw__d3ltxdb" href="#" target="_blank" rel="noopener noreferrer nofollow">Giới thiệu</a>
                 </li>
                 <li className="aw__d1fbfq7g">
-                  <a className="aw__d3ltxdb" href="https://trogiup.chotot.com/nguoi-ban/hoat-dong/" target="_blank" rel="noopener noreferrer nofollow">Quy chế hoạt động sàn</a>
+                  <a className="aw__d3ltxdb" href="#" target="_blank" rel="noopener noreferrer nofollow">Quy chế hoạt động sàn</a>
                 </li>
                 <li className="aw__d1fbfq7g">
-                  <a className="aw__d3ltxdb" href="https://trogiup.chotot.com/nguoi-ban/chinh-sach-bao-mat-cua-cho-tot/" target="_blank" rel="noopener noreferrer nofollow">Chính sách bảo mật</a>
+                  <a className="aw__d3ltxdb" href="#" target="_blank" rel="noopener noreferrer nofollow">Chính sách bảo mật</a>
                 </li>
                 <li className="aw__d1fbfq7g">
-                  <a className="aw__d3ltxdb" href="https://trogiup.chotot.com/nguoi-ban/co-che-giai-quyet-tranh-chap-cua-cho-tot/" target="_blank" rel="noopener noreferrer nofollow">Giải quyết tranh chấp</a>
+                  <a className="aw__d3ltxdb" href="#" target="_blank" rel="noopener noreferrer nofollow">Giải quyết tranh chấp</a>
                 </li>
                 <li className="aw__d1fbfq7g">
-                  <a className="aw__d3ltxdb" href="https://careers.chotot.com" target="_blank" rel="noopener noreferrer nofollow">Tuyển dụng</a>
+                  <a className="aw__d3ltxdb" href="#" target="_blank" rel="noopener noreferrer nofollow">Tuyển dụng</a>
                 </li>
                 <li className="aw__d1fbfq7g">
-                  <a className="aw__d3ltxdb" href="https://press.chotot.com" target="_blank" rel="noopener noreferrer nofollow">Truyền thông</a>
+                  <a className="aw__d3ltxdb" href="#" target="_blank" rel="noopener noreferrer nofollow">Truyền thông</a>
                 </li>
                 <li className="aw__d1fbfq7g">
-                  <a className="aw__d3ltxdb" href="https://www.chotot.com/kinh-nghiem" target="_blank" rel="noopener noreferrer nofollow">Blog</a>
+                  <a className="aw__d3ltxdb" href="#" target="_blank" rel="noopener noreferrer nofollow">Blog</a>
                 </li>
               </ul>
             </div>
@@ -84,36 +60,24 @@ const Footer = () => {
                 <p className="aw__h1klxoqy">Liên kết</p>
                 <ul className="aw__ue6m5w0">
                   <li className="aw__d1fbfq7g">
-                    <a className="aw__d3ltxdb" href="https://www.facebook.com/chotot.vn" target="_blank" rel="noopener noreferrer nofollow">
+                    <a className="aw__d3ltxdb" href="https://www.facebook.com/wanfreelancefotone" target="_blank" rel="noopener noreferrer nofollow">
                       <img className="aw__d1m9rhxg" alt="Facebook" width="32" height="32" src="https://static.chotot.com/storage/default/facebook.svg" />
                     </a>
                   </li>
                   <li className="aw__d1fbfq7g">
-                    <a className="aw__d3ltxdb" href="https://www.youtube.com/ChototVN" target="_blank" rel="noopener noreferrer nofollow">
+                    <a className="aw__d3ltxdb" href="https://www.facebook.com/wanfreelancefotone" target="_blank" rel="noopener noreferrer nofollow">
                       <img className="aw__d1m9rhxg" alt="Youtube" width="32" height="32" src="https://static.chotot.com/storage/default/youtube.svg" />
                     </a>
                   </li>
                   <li className="aw__d1fbfq7g">
-                    <a className="aw__d3ltxdb" href="https://linkedin.com/company/cho-tot" target="_blank" rel="noopener noreferrer nofollow">
+                    <a className="aw__d3ltxdb" href="https://www.facebook.com/wanfreelancefotone" target="_blank" rel="noopener noreferrer nofollow">
                       <img className="aw__d1m9rhxg" alt="LinkedIn" width="32" height="32" src="https://static.chotot.com/storage/default/linkedin.svg" />
                     </a>
                   </li>
                 </ul>
               </div>
 
-              <div className="aw__i1ce1r9u">
-                <div className="aw__h1klxoqy">Chứng nhận</div>
-                <ul className="aw__d2kbcgu">
-                  <li className="aw__d1fbfq7g">
-                    <a className="aw__d3ltxdb" target="_blank" rel="noopener noreferrer nofollow" href="http://online.gov.vn/Home/WebDetails/33119">
-                      <picture>
-                        <source type="image/webp" width="130" height="40" srcSet="https://static.chotot.com/storage/default/certificate.webp" />
-                        <img alt="Certification" width="130" height="40" src="https://static.chotot.com/storage/default/cerfiticate.png" />
-                      </picture>
-                    </a>
-                  </li>
-                </ul>
-              </div>
+             
             </div>
           </section>
         </div>
@@ -121,13 +85,13 @@ const Footer = () => {
         <hr className="aw__h1thvigh" />
 
         {/* Company Information */}
-        <section className="aw__c1klqbjn">
+        {/* <section className="aw__c1klqbjn">
           <address className="aw__d1jlhxju">
-          CÔNG TY TNHH CHỢ TỐT - Người đại diện theo pháp luật: Nguyễn Trọng Tấn; GPDKKD: 0312120782 do sở KH & ĐT TP.HCM cấp ngày 11/01/2013;
-GPMXH: 185/GP-BTTTT do Bộ Thông tin và Truyền thông cấp ngày 09/07/2024 - Chịu trách nhiệm nội dung: Trần Hoàng Ly. Chính sách sử dụng
-Địa chỉ: Tầng 18, Toà nhà UOA, Số 6 đường Tân Trào, Phường Tân Phú, Quận 7, Thành phố Hồ Chí Minh, Việt Nam; Email: trogiup@chotot.vn - Tổng đài CSKH: 19003003 (1.000đ/phút)
+            CÔNG TY TNHH CHỢ TỐT - Người đại diện theo pháp luật: Nguyễn Trọng Tấn; GPDKKD: 0312120782 do sở KH & ĐT TP.HCM cấp ngày 11/01/2013;
+            GPMXH: 185/GP-BTTTT do Bộ Thông tin và Truyền thông cấp ngày 09/07/2024 - Chịu trách nhiệm nội dung: Trần Hoàng Ly. Chính sách sử dụng
+            Địa chỉ: Tầng 18, Toà nhà UOA, Số 6 đường Tân Trào, Phường Tân Phú, Quận 7, Thành phố Hồ Chí Minh, Việt Nam; Email: trogiup@chotot.vn - Tổng đài CSKH: 19003003 (1.000đ/phút)
           </address>
-        </section>
+        </section> */}
       </div>
     </footer>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ const Header = () => {
             <div className="header__app-wrapper">
                 {/* Logo Section */}
                 <div className="header__logo-container">
-                    <a className="header__logo-link" href="#">
+                    <a className="header__logo-link" href="https://www.facebook.com/wanfreelancefotone">
                         <picture>
 
                             <img
