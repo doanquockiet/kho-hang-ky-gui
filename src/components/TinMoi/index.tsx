be22daf6221d330bdebdb808e6fe5cb0897a24a6
Doan Quoc Kiet <116879984+doanquockiet@users.noreply.github.com>
2024-10-14 20:06:15 +0700

    update image

--- src/components/TinMoi/index.tsx
+++ src/components/TinMoi/index.tsx
@@ -1,22 +1,28 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import productImage from '../../assets/sanpham/sanpham2.svg';
 import './TinMoi.css';
-
+import ao1 from '../../assets/sanpham/ao1.jpg'
+import ao2 from '../../assets/sanpham/ao2.jpg'
+import ao3 from '../../assets/sanpham/ao3.jpg'
+import ao4 from '../../assets/sanpham/ao4.jpg'
+import ao5 from '../../assets/sanpham/clmgiz.jpg'
+import quan1 from '../../assets/sanpham/quan1.jpg'
+import quan2 from '../../assets/sanpham/quan2.jpg'
 const Tinmoi = () => {
   const products = [
-    { id: 1, name: 'Sản phẩm 1', price: '1.000.000đ', location: 'Vĩnh Long', time: '24 Giờ Trước' },
-    { id: 2, name: 'Sản phẩm 2', price: '2.000.000đ', location: 'Cần Thơ', time: '12 Giờ Trước' },
-    { id: 3, name: 'Sản phẩm 3', price: '3.000.000đ', location: 'Hồ Chí Minh', time: '1 Giờ Trước' },
-    { id: 4, name: 'Sản phẩm 4', price: '4.000.000đ', location: 'Hà Nội', time: '3 Giờ Trước' },
-    { id: 5, name: 'Sản phẩm 5', price: '5.000.000đ', location: 'Đà Nẵng', time: '6 Giờ Trước' },
-    { id: 6, name: 'Sản phẩm 6', price: '6.000.000đ', location: 'Huế', time: '2 Giờ Trước' },
-    { id: 8, name: 'Sản phẩm 7', price: '8.000.000đ', location: 'Huế', time: '2 Giờ Trước' },
-    { id: 9, name: 'Sản phẩm 8', price: '6.000.000đ', location: 'Huế', time: '2 Giờ Trước' },
-    { id: 10, name: 'Sản phẩm 9', price: '9.000.000đ', location: 'Huế', time: '2 Giờ Trước' },
-    { id: 11, name: 'Sản phẩm 10', price: '6.000.000đ', location: 'Huế', time: '2 Giờ Trước' },
-    { id: 12, name: 'Sản phẩm 11', price: '6.000.000đ', location: 'Huế', time: '2 Giờ Trước' },
-    { id: 13, name: 'Sản phẩm 12', price: '3.000.000đ', location: 'Huế', time: '2 Giờ Trước' },
-    { id: 14, name: 'Sản phẩm 13', price: '7.000.000đ', location: 'Huế', time: '2 Giờ Trước' },
+    { id: 1, name: 'Sản phẩm 1', price: '1.000.000đ', location: 'Vĩnh Long', time: '24 Giờ Trước',image: ao1 },
+    { id: 2, name: 'Sản phẩm 2', price: '2.000.000đ', location: 'Cần Thơ', time: '12 Giờ Trước',image: ao2 },
+    { id: 3, name: 'Sản phẩm 3', price: '3.000.000đ', location: 'Hồ Chí Minh', time: '1 Giờ Trước',image: ao3 },
+    { id: 4, name: 'Sản phẩm 4', price: '4.000.000đ', location: 'Hà Nội', time: '3 Giờ Trước',image: ao4 },
+    { id: 5, name: 'Sản phẩm 5', price: '5.000.000đ', location: 'Đà Nẵng', time: '6 Giờ Trước',image: ao5 },
+    { id: 6, name: 'Sản phẩm 6', price: '6.000.000đ', location: 'Huế', time: '2 Giờ Trước',image: quan1 },
+    { id: 8, name: 'Sản phẩm 7', price: '8.000.000đ', location: 'Huế', time: '2 Giờ Trước',image: quan2 },
+    { id: 9, name: 'Sản phẩm 8', price: '6.000.000đ', location: 'Huế', time: '2 Giờ Trước',image: ao1 },
+    { id: 10, name: 'Sản phẩm 9', price: '9.000.000đ', location: 'Huế', time: '2 Giờ Trước',image: ao4 },
+    { id: 11, name: 'Sản phẩm 10', price: '6.000.000đ', location: 'Huế', time: '2 Giờ Trước',image: ao1 },
+    { id: 12, name: 'Sản phẩm 11', price: '6.000.000đ', location: 'Huế', time: '2 Giờ Trước',image: ao1 },
+    { id: 13, name: 'Sản phẩm 12', price: '3.000.000đ', location: 'Huế', time: '2 Giờ Trước',image: ao1 },
+    { id: 14, name: 'Sản phẩm 13', price: '7.000.000đ', location: 'Huế', time: '2 Giờ Trước',image: ao1 },
   ];
 
   return (
@@ -30,7 +36,7 @@ const Tinmoi = () => {
             <a href="#">
               <div className="product-item">
                 <div className="image-product">
-                  <img src={productImage} alt={product.name} />
+                  <img src={product.image} alt={product.name} />
                 </div>
                 <div className="infor-product">
                   <p>{product.name}</p>
