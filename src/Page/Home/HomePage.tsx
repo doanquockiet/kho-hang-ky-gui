4c3888a5344338611f494ea3debef5b97b7cff4f
Doan Quoc Kiet <116879984+doanquockiet@users.noreply.github.com>
2024-10-15 10:20:58 +0700

    UPDATE:: update price product

--- src/Page/Home/HomePage.tsx
+++ src/Page/Home/HomePage.tsx
@@ -1,8 +1,6 @@
-
 import Banner from '../../components/Banner'
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header'
-
 import Tinmoi from '../../components/TinMoi'
 
 
@@ -17,5 +15,4 @@ const HomePage = () => {
     </div>
   )
 }
-// asdfghjkl
 export default HomePage
