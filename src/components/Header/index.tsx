3562101c7593a325ec1f6845100eda7412bef93a
Doan Quoc Kiet <116879984+doanquockiet@users.noreply.github.com>
2024-10-21 15:11:12 +0700

    update:: link home page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,7 @@ const Header = () => {
             <div className="header__app-wrapper">
                 {/* Logo Section */}
                 <div className="header__logo-container">
-                    <a className="header__logo-link" href="https://www.facebook.com/wanfreelancefotone">
+                    <a className="header__logo-link" href="/">
                         <picture>
 
                             <img
